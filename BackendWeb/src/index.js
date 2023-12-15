4077a2507ca2090e9af19b63fa4d42bd8469e894
DoAnCDTT <snownghi3979@gmail.com>
2023-12-15 19:02:32 +0700

    ThucTapNghi

---
+++ BackendWeb/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');  // Đã sửa thành require('mongoose')
+const routes = require('./routes');
+ const cors = require('cors'); // bao mat neu truy cap cac egmail ! nhau 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors())
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+app.use(bodyParser.json ())
+app.use(cookieParser ())
+routes(app);
+
+
+mongoose.connect(`${process.env.MONGO_DB}`)
+  .then(() => {
+    console.log('Da ket noi voi MongoDB');
+  })
+  .catch((err) => {
+    console.log( err);
+  });
+app.listen(port, () => {
+  console.log('Dang chay cong:', + port);
+});
